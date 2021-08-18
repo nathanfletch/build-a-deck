7c4b56935ae63df46391dd233c14897d5e0013e6
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-08-18 06:49:32 -0700

    Fix an omission in the cardNames array
    Co-authored-by: Paige Tiedeman <paigetiedeman@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-  const cardNames = ['2', '3', '4', '5', '6', '7', '8', '9', 'Jack', 'Queen', 'King', 'Ace'];
+  const cardNames = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
   const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
 
   suits.forEach(function(suit) {
 
 
     $('#list').append("<h3>" + suit + "</h3>");
-    
+    g
 
     cardNames.forEach(function(cardName) {
 
       $('#list').append($('<li>').text(cardName + " of " + suit));
   
     });
+
     $('#list').append('<br>');
   })
   
